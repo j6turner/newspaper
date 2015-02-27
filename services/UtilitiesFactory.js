ab64eef74dcd320a748e682c3e466c7857001bfe
J. TURNER <turner.j6@gmail.com>
2015-02-27 13:03:16 -0800

    Add missing opening curly brace.

--- services/UtilitiesFactory.js
+++ services/UtilitiesFactory.js
@@ -1,6 +1,6 @@
 newspaperMaintainer.factory("UtilitiesFactory", function() {
   return {
-    findByID: function(set, id)
+    findByID: function(set, id) {
       for (var i = 0; i < set.length; i++) {
         if (set[i].id == id)
           return set[i];
