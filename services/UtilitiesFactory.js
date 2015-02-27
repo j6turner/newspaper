d983ef38e49a209a3ac17a489bd6f954ed57b1bf
J. TURNER <turner.j6@gmail.com>
2015-02-27 12:28:33 -0800

    Establish services/UtilitiesFactory.js; create findByID method.

---
+++ services/UtilitiesFactory.js
@@ -0,0 +1,11 @@
+newspaperMaintainer.factory("UtilitiesFactory", function() {
+  return {
+    findByID: function(set, id)
+      for (var i = 0; i < set.length; i++) {
+        if (set[i].id == id)
+          return set[i];
+      }
+      return null;
+    }
+  };
+});
