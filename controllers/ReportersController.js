d6e7fd95ed5a558e7f58c903f7cbd651ac077097
J. TURNER <turner.j6@gmail.com>
2015-02-27 13:02:15 -0800

    Adjust spacing.

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,10 +1,10 @@
 newspaperMaintainer.controller("ReportersCtrl",
   function ReportersCtrl($scope, $stateParams, SectionsFactory, UtilitiesFactory) {
-    $scope.section = UtilitiesFactory.findByID(SectionsFactory.sections, $stateParams.sectionID)
-    $scope.addReporter = function() {
-      $scope.section.reporters.push({
-        name: $scope.reporterName
-      });
-      $scope.reporterName = null;
-    }
-  });
+  $scope.section = UtilitiesFactory.findByID(SectionsFactory.sections, $stateParams.sectionID)
+  $scope.addReporter = function() {
+    $scope.section.reporters.push({
+      name: $scope.reporterName
+    });
+    $scope.reporterName = null;
+  }
+});
--- controllers/SectionsController.js
+++ controllers/SectionsController.js
@@ -2,4 +2,4 @@ newspaperMaintainer.controller("SectionsCtrl",
   function SectionsCtrl($scope, SectionsFactory) {
     $scope.sections = SectionsFactory.sections;
     $scope.SectionsFactory = SectionsFactory;
-  });
+});
