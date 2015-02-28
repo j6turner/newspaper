234683d5a04fb9fc21e1fcad2973dd7bf0749753
J. TURNER <turner.j6@gmail.com>
2015-02-27 17:04:42 -0800

    Add details for reporter input.

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -3,8 +3,12 @@ newspaperMaintainer.controller("ReportersCtrl",
   $scope.section = UtilitiesFactory.findByID(SectionsFactory.sections, $stateParams.sectionID)
   $scope.addReporter = function() {
     $scope.section.reporters.push({
-      name: $scope.reporterName
+      name: $scope.reporterName,
+      employment: $scope.reporterEmployment,
+      birthday: $scope.reporterBirthday
     });
     $scope.reporterName = null;
+    $scope.reporterEmployement = null;
+    $scope.reporterBirthday = null;
   }
 });
